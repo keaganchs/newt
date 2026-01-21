74370c715629e4cd705af4984cc0817103eb3618
Keagan Holmes <holmes@steinbuch.ias.informatik.tu-darmstadt.de>
2026-01-21 18:55:10 +0100

    Update experiment

--- experiments/train_trm_cluster.sh
+++ experiments/train_trm_cluster.sh
@@ -11,6 +11,10 @@
 #SBATCH --time=23:59:00
 #SBATCH --gres=gpu:1
 
+
+# If using Slurm, run this script from the root of the repository!
+
+
 # Activate conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate newt
@@ -22,10 +26,10 @@ PYTHON_SCRIPT="$SCRIPT_DIR/../tdmpc2/train.py"
 # Run
 python3 $PYTHON_SCRIPT \
     task="mujoco" \
-    use_trm_encoder=False \
+    use_trm_encoder=True \
     obs="state" \
-    model_size="B" \
+    model_size="S" \
     wandb_project="newt_trm" \
     wandb_entity="keagan" \
-    wandb_run_name="mlp_baseline" \
+    wandb_run_name="trm_mlp_s_0" \
     enable_wandb=True \
