7b19002e1cd04371eb00f553b72720000cebd5a8
Keagan <keaganchs@gmail.com>
2026-04-20 14:52:14 +0200

    Update experiment

--- experiments/paper/newt_m_l.sh
+++ experiments/paper/newt_m_l.sh
@@ -3,6 +3,7 @@
 PROJECT_ROOT="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
 
 echo "Running Newt M experiments from ${PROJECT_ROOT}"
+cd "${PROJECT_ROOT}"
 
-sbatch "${PROJECT_ROOT}/experiments/paper/newt_m_l/newt_m.sh"
-sbatch "${PROJECT_ROOT}/experiments/paper/newt_m_l/newt_l.sh"
+sbatch "experiments/paper/newt_m_l/newt_m.sh"
+sbatch "experiments/paper/newt_m_l/newt_l.sh"
