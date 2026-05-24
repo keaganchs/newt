30f5ee9c2180fb8711f57829444d33e6f908e330
Keagan <keaganchs@gmail.com>
2026-05-24 10:59:52 +0200

    Add gradnorm experiments

---
+++ experiments/paper/gradnorm/trm.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#SBATCH --job-name=newt_trm
+#SBATCH --output=log/out_and_err_%j.txt
+#SBATCH --error=log/out_and_err_%j.txt
+#SBATCH --partition=amd2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=9
+#SBATCH --mem-per-cpu=5500
+#SBATCH --time=71:59:00
+#SBATCH --gres=gpu:1
+
+
+# If using Slurm, run this script from the root (.../newt) directory of the repository!
+
+
+# Activate conda environment
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+conda activate newt
+
+# Make paths
+cd $SLURM_SUBMIT_DIR
+
+SCRIPT_DIR="$SLURM_SUBMIT_DIR"
+PYTHON_SCRIPT="$SCRIPT_DIR/tdmpc2/train.py"
+
+SEEDS=(0 1 2)
+
+for SEED in "${SEEDS[@]}"; do
+    python3 $PYTHON_SCRIPT \
+        task="dmcontrol" \
+        num_envs=21 \
+        use_trm_encoder=False \
+        use_trm_dynamics="trm" \
+        use_task_embedding=True \
+        obs="state" \
+        model_size="S" \
+        mlp_t=True \
+        halt_max_steps=0 \
+        halt_exploration_prob=0 \
+        wandb_project="TRM Dynamics" \
+        wandb_entity="trm-dynamics" \
+        wandb_run_name="dmc_trmd_s_s${SEED}_128hs384ld_128opt_2h6l_gradnorm" \
+        enable_wandb=True \
+        H_cycles=2 \
+        L_cycles=6 \
+        num_state_obs_per_token=128 \
+        pooling_strategy="mean_obs_only" \
+        hidden_size=128 \
+        num_heads=2 \
+        seed="$SEED" \
+        latent_dim=384 &
+done
+
+wait
