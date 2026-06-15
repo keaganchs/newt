33a3073d33ad4b9c04155fd7001d1dedbdcff160
Keagan Holmes <holmes@steinbuch.ias.informatik.tu-darmstadt.de>
2026-06-15 11:38:16 +0200

    Add 42 cluster experiment

---
+++ experiments/42_run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Activate conda environment
+export LD_LIBRARY_PATH="/pfss/mlde/workspaces/mlde_wsp_PI_Deramo/miniconda3/lib:$LD_LIBRARY_PATH"
+eval "$(/pfss/mlde/workspaces/mlde_wsp_PI_Deramo/miniconda3/bin/conda shell.bash hook)"
+conda activate newt
+
+PROJECT_ROOT="$(dirname "$(dirname "$(realpath "$0")")")"
+PYTHON_SCRIPT="${PROJECT_ROOT}/tdmpc2/train.py"
+cd "${PROJECT_ROOT}"
+echo "Using Python script: ${PYTHON_SCRIPT}"
+
+SEEDS=(0 1 2)
+
+for SEED in "${SEEDS[@]}"; do
+    python3 $PYTHON_SCRIPT \
+        task="dmcontrol" \
+        num_envs=21 \
+        use_trm_encoder=False \
+        use_trm_dynamics="simple" \
+        use_task_embedding=True \
+        obs="state" \
+        model_size="S" \
+        wandb_project="TRM Dynamics" \
+        wandb_entity="trm-dynamics" \
+        wandb_run_name="smp_s${SEED}_384ld_2h6l_sisw_maskx_film" \
+        enable_wandb=True \
+        H_cycles=2 \
+        L_cycles=6 \
+        seed="$SEED" \
+        latent_dim=384 \
+        use_film_dynamics=True \
+        use_simple_trm_skip_connections=True \
+        simple_trm_skip_type="swiglu" \
+        rrm_mask_x_for_y_update=True &
+done
+
+for SEED in "${SEEDS[@]}"; do
+    python3 $PYTHON_SCRIPT \
+        task="dmcontrol" \
+        num_envs=21 \
+        use_trm_encoder=False \
+        use_trm_dynamics="simple" \
+        use_task_embedding=True \
+        obs="state" \
+        model_size="S" \
+        wandb_project="TRM Dynamics" \
+        wandb_entity="trm-dynamics" \
+        wandb_run_name="smp_s${SEED}_384ld_2h6l_sisw_maskx_nofilm" \
+        enable_wandb=True \
+        H_cycles=2 \
+        L_cycles=6 \
+        seed="$SEED" \
+        latent_dim=384 \
+        use_film_dynamics=False \
+        use_simple_trm_skip_connections=True \
+        simple_trm_skip_type="swiglu" \
+        rrm_mask_x_for_y_update=True &
+done
+
+wait
