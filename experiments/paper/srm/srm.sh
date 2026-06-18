6f304f54f9338366751c9f09061d9abada46b34e
Keagan Holmes <holmes@steinbuch.tail51ec2d.ts.net>
2026-06-18 17:22:17 +0200

    Add SRM experiment

---
+++ experiments/paper/srm/srm.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#SBATCH --job-name=newt_trm
+#SBATCH --output=log/out_and_err_%j.txt
+#SBATCH --error=log/out_and_err_%j.txt
+#SBATCH --partition=amd2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=9
+#SBATCH --mem-per-cpu=5500
+#SBATCH --time=71:59:00
+#SBATCH --gres=gpu:1
+
+
+# If using Slurm, run this script from the root (.../newt) directory of the repository!
+
+
+# Activate conda environment
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+conda activate newt
+
+# Make paths
+cd $SLURM_SUBMIT_DIR
+
+SCRIPT_DIR="$SLURM_SUBMIT_DIR"
+PYTHON_SCRIPT="$SCRIPT_DIR/tdmpc2/train.py"
+
+SEEDS=(0)
+
+for SEED in "${SEEDS[@]}"; do
+    python3 $PYTHON_SCRIPT \
+        task="dmcontrol" \
+        num_envs=21 \
+        use_trm_encoder=False \
+        use_trm_dynamics="srm" \
+        obs="state" \
+        model_size="S" \
+        wandb_project="TRM Dynamics" \
+        wandb_entity="trm-dynamics" \
+        wandb_run_name="srm_s${SEED}_16ld_4h3l_xl_nofilm" \
+        enable_wandb=True \
+        H_cycles=4 \
+        L_cycles=3 \
+        seed="$SEED" \
+        latent_dim=16 \
+        use_film_dynamics=False \
+        srm_truncation_length=3 &
+done
+
+for SEED in "${SEEDS[@]}"; do
+    python3 $PYTHON_SCRIPT \
+        task="dmcontrol" \
+        num_envs=21 \
+        use_trm_encoder=False \
+        use_trm_dynamics="srm" \
+        obs="state" \
+        model_size="S" \
+        wandb_project="TRM Dynamics" \
+        wandb_entity="trm-dynamics" \
+        wandb_run_name="srm_s${SEED}_16ld_4h3l_xl_film" \
+        enable_wandb=True \
+        H_cycles=4 \
+        L_cycles=3 \
+        seed="$SEED" \
+        latent_dim=16 \
+        use_film_dynamics=True \
+        srm_truncation_length=3 &
+done
+
+wait
